a3b9d7af21642d726f50e69227bd7f07c13a032f
Ayushh-g <122664197+Ayushh-g@users.noreply.github.com>
2023-04-20 22:00:25 +0530

    Add files via upload

---
+++ practice4.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_csv("ECG_training (org).csv",chunksize=2)
+for chunk in data:
+    # data=chunk[0].str.split(",",expand=True)
+    X=chunk['ECG'].str.split(',',expand=True)
+    y=chunk['Classification']
+    X=np.array(X,dtype=float)
+    X_A=X[y=='A']
+    X_N=X[y=='N']
+    plt.plot(X_A.T)
+    plt.title('class a')
+    plt.show()
+    plt.plot(X_N.T)
+    plt.title('class n')
+    plt.show()
+    break
